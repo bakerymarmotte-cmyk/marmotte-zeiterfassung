a3a18118239015c746752575453c09f45ee8f4c5
bakerymarmotte-cmyk <bakery.marmotte@gmail.com>
2026-07-13 00:26:36 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Sorgt dafür, dass die App auf dem Smartphone als "installierbar" erkannt wird
 // und die Grundseiten auch bei kurzzeitig fehlender Verbindung geladen werden können.
 
-const CACHE_NAME = "marmotte-shell-v5";
+const CACHE_NAME = "marmotte-shell-v6";
 const SHELL_FILES = [
   "./index.html",
   "./style.css",
